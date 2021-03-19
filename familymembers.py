9ae08d64ce5c6f624886479a74dd2a2125a91f97
venkateshmunasala <venkateshmunasla@users.noreply.github.com>
2021-03-19 10:36:40 +0530

    family persons

--- familymembers.py
+++ familymembers.py
@@ -6,3 +6,4 @@ print('my name is venkatesh')
 print('s/o appalachari,m/o kanakarathnam')
 
 print('thotakurapalem,ravikamatham,visakahapatnam')
+print('family pesons ,appalachari,kananaratham,meghani,priya,venkatesh')
